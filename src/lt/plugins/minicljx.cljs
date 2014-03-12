49c0f79e2838b63f105c5335567a357a852da179
Scott Rabin <scott@huddler-inc.com>
2014-03-11 17:00:21 -0700

    Evaluate one form at a time now works as expected

--- src/lt/plugins/minicljx.cljs
+++ src/lt/plugins/minicljx.cljs
@@ -1,5 +1,6 @@
 (ns lt.plugins.minicljx
   (:require [lt.plugins.clojure :as lt-clj]
+            [lt.plugins.watches :as watches]
             [cljs.reader :as reader]
             [lt.util.cljs :refer [->dottedkw str-contains?]]
             [lt.object :as object]
@@ -20,18 +21,18 @@
                                   :create lt-clj/try-connect})]
     (clients/send client command info :only editor)))
 
-(defn get-transform [code ruleset]
+(defn- get-transform [code ruleset]
   (-> "(require '[cljx.core] '[cljx.rules]) (cljx.core/transform %CODE% cljx.rules/%RULESET%-rules)"
       (clojure.string/replace "%RULESET%" (name ruleset))
       (clojure.string/replace "%CODE%" (pr-str code))))
 
-(defn do-transform
+(defn- do-transform
   "Run the transform operations for clj/s"
-  [editor code ruleset]
+  [editor info ruleset]
   (send-command editor :editor.eval.clj
-                (assoc (:info @editor)
-                  :code (get-transform code ruleset)
-                  :meta {::type ruleset})))
+                (-> info
+                    (assoc :code (get-transform (:code info) ruleset))
+                    (assoc-in [:meta ::type] ruleset))))
 
 (defn- process-cljx-transform-result
   "Internal helper function to process the result of a cljx transformation"
@@ -42,7 +43,9 @@
                             (map :result)
                             (map reader/read-string)
                             (apply str))
-        info (assoc (:info @editor) :code processed-code)]
+        info (assoc (:info @editor)
+               :code processed-code
+               :pos (get-in result [:meta ::pos]))]
     (send-command editor command info)))
 
 (defn- process-cljx-result
@@ -53,10 +56,19 @@
     (object/raise editor command result)))
 
 (behavior ::on-eval
-          :triggers #{:eval :eval.one}
+          :triggers #{:eval}
           :reaction (fn [editor]
-                      (let [code (ed/->val (:ed @editor))]
-                        (do-transform editor code :clj))))
+                      (let [info (assoc (:info @editor)
+                                   :code (ed/->val (:ed @editor)))]
+                        (do-transform editor info :clj))))
+
+(behavior ::on-eval.one
+          :triggers #{:eval.one}
+          :reaction (fn [editor]
+                      (let [info (assoc (:info @editor)
+                                   :code (ed/->val (:ed @editor))
+                                   :meta {::pos (ed/->cursor editor)})]
+                      (do-transform editor info :clj))))
 
 (behavior ::cljx-result
           :triggers #{:editor.eval.clj.result}
@@ -88,3 +100,8 @@
                                  :start-line (dec (get-in res [:meta :line] 1))}]
                         (notifos/set-msg! (:result res) {:class "error"})
                         (object/raise obj :editor.exception (:stack res) loc))))
+
+(behavior ::cljx-result.no-op
+          :triggers #{:editor.eval.clj.no-op}
+          :reaction (fn [editor location]
+                      (notifos/set-msg! "No form found under cursor" {:class "error"}) ))
